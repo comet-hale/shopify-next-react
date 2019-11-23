0ec6074fd1979751dfc7fc7c8190131d9096cfd2
geekfreelancer <johnsmith93030@gmail.com>
2019-11-23 17:40:09 +0000

    Add verify middleware && install dev store

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require(isomorphic-fetch);
+require('isomorphic-fetch');
 const dotenv = require('dotenv');
 const Koa = require('koa');
 const next = require('next');
@@ -9,10 +9,39 @@ const session = require('koa-session');
 dotenv.config();
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE-ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const { SHOPIFY_API_KEY, SHOPIFY_SECRET_API_KEY } = process.env;
 
-app.prepare().then(() => {});
+app.prepare().then(() => {
+    const server = new Koa();
+    server.use(session(server));
+    server.keys = SHOPIFY_SECRET_API_KEY;
+
+    server.use(
+        createShopifyAuth({
+            apiKey: SHOPIFY_API_KEY,
+            secret: SHOPIFY_SECRET_API_KEY,
+            scopes: ['read_products'],
+            afterAuth(ctx) {
+                const { shop, accessToken } = ctx.session;
+                ctx.redirect('/');
+            }
+        }),
+    );
+
+    server.use(verifyRequest());
+
+    server.use(async (ctx) => {
+        await handle(ctx.req, ctx.res);
+        ctx.respond = false;
+        ctx.res.statusCode = 200;
+        return;
+    });
+
+    server.listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`);
+    });
+});
