c9248f054ac5276f8841e2c930efd86a6c2377a6
geekfreelancer <johnsmith93030@gmail.com>
2019-11-23 19:02:07 +0000

    Add shopify bridge

--- pages/_app.js
+++ pages/_app.js
@@ -2,21 +2,31 @@ import App from 'next/app'; // let us not to add import to each files
 import Head from 'next/head';
 // Add the polaris app provider component, so to be wrapped to use polaris
 import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
+import Cookies from 'js-cookie';
 
 class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props;
+        const config = {
+            apiKey: API_KEY,
+            shopOrigin: Cookies.get("shopOrigin"),
+            forceRedirect: true,
+        };
+
         return (
             <React.Fragment>
                 <Head>
                     <title>Sample App</title>
                     <meta charSet='utf-8' />
                 </Head>
-                <AppProvider i18n={translations}>
-                    <Component { ...pageProps } />
-                </AppProvider>
+                <Provider config={config}>
+                    <AppProvider i18n={translations}>
+                        <Component { ...pageProps } />
+                    </AppProvider>
+                </Provider>
             </React.Fragment>
         );
     }
--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,47 @@ import {
     Page,
     EmptyState,
 } from '@shopify/polaris';
+import { TitleBar, ResourcePicker } from '@shopify/app-bridge-react';
 
 const imgURL = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
-const Index = () => (
-    <Page>
-        <Layout>
-            <EmptyState
-                heading="Discount your products"
-                action={{
-                    content: 'Select products',
-                    onAction: () => console.log('clicked')
-                }}
-                image={imgURL}
-            >
-                <p>Select products to change their price</p>
-            </EmptyState>
-        </Layout>
-    </Page>
-);
+class Index extends React.Component {
+    state = { open: false };
+    render() {
+        return (
+            <Page>
+                <TitleBar
+                    primaryAction={{
+                        content: 'Select products',
+                    }}
+                />
+                <ResourcePicker
+                    resourceType="Product"
+                    showVariants={false}
+                    open={this.state.open}
+                    onSelection={(resources) => this.handleSelection(resources)}
+                    onCancel={() => this.setState({ open: false })}
+                />
+                <Layout>
+                    <EmptyState
+                        heading="Discount your products"
+                        action={{
+                            content: 'Select products',
+                            onAction: () => this.setState({ open: true }),
+                        }}
+                        image={imgURL}
+                    >
+                        <p>Select products to change their price</p>
+                    </EmptyState>
+                </Layout>
+            </Page>
+        );
+    }
+    handleSelection = (resources) => {
+        const idsFromResources = resources.selection.map((product) => product.id);
+        this.setState({ open: false });
+        console.log(resources);
+    };
+}
 
 export default Index;
